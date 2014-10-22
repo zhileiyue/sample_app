840ab1387572cc96748336bb400a0fda736ef1b6
zhileiyue <zhilei.yue@gmail.com>
2014-10-22 10:48:15 -0400

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
 
  def create
   user = User.find_by(email: params[:session][:email].downcase)
- 
+
   if user && user.authenticate(params[:session][:password])
 
   # Sign the user in and redirect to the user's show page.
    sign_in user
-   redirect_to user
+   redirect_back_or user
 
   else
   
